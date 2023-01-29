a4418eff11dc3ae0b7063d8d1bdbf64e5418369b
TON RUGERIA <tonrugeria@gmail.com>
2023-01-30 02:40:33 +0800

    Create Group Chat
    Add user with minimum 3 users to create a group chat
    add or remove user
    rename the group chat name
    leave the group chat

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -4,9 +4,10 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { getSender } from "../config/ChatLogics";
 import { ChatState } from "../Context/ChatProvider";
+import GroupChatModal from "./Authentication/misc/GroupChatModal";
 import ChatLoading from "./ChatLoading";
 
-const MyChats = () => {
+const MyChats = ({ fetchAgain }) => {
   const [loggedUser, setLoggedUser] = useState();
   const { selectedChat, setSelectedChat, user, chats, setChats } = ChatState();
 
@@ -38,7 +39,7 @@ const MyChats = () => {
   useEffect(() => {
     setLoggedUser(JSON.parse(localStorage.getItem("userInfo")));
     fetchChats();
-  }, []);
+  }, [fetchAgain]);
 
   return (
     <Box
@@ -62,18 +63,18 @@ const MyChats = () => {
         alignItems="center"
       >
         My Chats
-        {/* <GroupChatModal> */}
-        <Button
-          d="flex"
-          fontSize={{ base: "17px", md: "10px", lg: "17px" }}
-          rightIcon={<AddIcon />}
-        >
-          New Group Chat
-        </Button>
-        {/* </GroupChatModal> */}
+        <GroupChatModal>
+          <Button
+            d="flex"
+            fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+            rightIcon={<AddIcon />}
+          >
+            New Group Chat
+          </Button>
+        </GroupChatModal>
       </Box>
       <Box
-        d="flex"
+        display="flex"
         flexDir="column"
         p={3}
         bg="#F8F8F8"
