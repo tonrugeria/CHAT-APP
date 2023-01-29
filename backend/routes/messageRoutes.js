85613c7bff418833133b8bc8396c9c42082f3840
TON RUGERIA <tonrugeria@gmail.com>
2023-01-30 03:09:25 +0800

    Added Message Routes and Controllers

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const {
+  sendMessage,
+  allMessages,
+} = require("../controllers/messageControllers");
+// const {
+//   allMessages,
+//   sendMessage,
+// } = require("../controllers/messageControllers");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+router.route("/:chatId").get(protect, allMessages);
+router.route("/").post(protect, sendMessage);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -31,6 +32,7 @@ app.get("/", (req, res) => {
 //The app.use() function is used to mount the specified middleware function
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
